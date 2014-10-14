ef6aedb713273d36d193451e449bbb10a31c8385
adityav <aditya.vadaganadam@gmail.com>
2014-10-14 12:37:50 +0100

    update

--- amazon-ex2/features/amazon-stepdefs.rb
+++ amazon-ex2/features/amazon-stepdefs.rb
@@ -1,39 +1,42 @@
 require 'watir-webdriver'
 require 'pry'
 
-Given(/^I opened the amazon web URL$/) do
-  #$browser = Watir::Browser.new :chrome
-  #$browser.goto "http://www.amazon.co.uk/"	  
-end
+
+  Given(/^I opened the amazon web URL$/) do
+    #$browser = Watir::Browser.new :chrome
+    #$browser.goto "http://www.amazon.co.uk/"	  
+  end
+
+  When(/^I search for 'Kindle Fire'$/) do
+    $browser.text_field(:id => 'twotabsearchtextbox').set("Kindle Fire")
+    $browser.button(:value => 'Go').click
+  end
+  Then(/^I should see atleast (\d+) result$/) do |expectedcount|
+    strFullResult =  $browser.h2(:id => 's-result-count').text
+    #binding.pry
+    indexStart = strFullResult.index('of ')
+    indexEnd = strFullResult.index(' results')
+    strActualResult = strFullResult [indexStart+3,indexEnd-8]  
+    countActualResult = strActualResult.gsub(',','').to_i
+    puts countActualResult
+    if  countActualResult >= expectedcount.to_i
+     	puts 'Multiple Results - Test Pass'	
+    end       
+  end
 
 
-When(/^I search for 'Kindle Fire'$/) do
-  $browser.text_field(:id => 'twotabsearchtextbox').set("Kindle Fire")
+When(/^I search for 'Negative Test Search String on Fire'$/) do
+  $browser.text_field(:id => 'twotabsearchtextbox').set("Negative Test Search String on Fire")
   $browser.button(:value => 'Go').click
 end
 
-Then(/^I should see atleast (\d+) result$/) do |expectedcount|
-  
-  puts $browser.h2(:id => 's-result-count').text
-  strFullResult =  $browser.h2(:id => 's-result-count').text
-  
-  #binding.pry
-  
-  indexStart = strFullResult.index('of ')
-  indexEnd = strFullResult.index(' results')
-   
- strActualResult = strFullResult [indexStart+3,indexEnd-8]  
- countActualResult = strActualResult.gsub(',','').to_i
- 
- puts countActualResult
-  
- if  countActualResult >= expectedcount.to_i
-	puts 'Multiple Results - Test Pass'	
- end
-  
-  
-  #actualresultcount
-  #expectedcount
+Then(/^I should see  (\d+) results$/) do |expectedcount2|
+    strNoResult =  $browser.h1(:id => 'noResultsTitle').text
     
-end
+    #binding.pry
 
+    if strNoResult.include? "did not match any products"
+        puts "Test Pass - No reslts found"
+    end    
+
+end
--- amazon-ex2/features/test.feature
+++ amazon-ex2/features/test.feature
@@ -9,6 +9,7 @@ Then I should see atleast 1 result
 Scenario: Check Amazon web for a product-negative test
 
 Given I opened the amazon web URL
-When I search for 'Negative Test Search String on Fire'
-Then I should see atleast 0 result
+When I search for "Negative Test Search String on Fire"
+Then I should see  0 results
+
 
